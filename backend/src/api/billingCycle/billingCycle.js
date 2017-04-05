043bc755ec3ab9760f23caaf04be4b897f72afc1
Leonardo Andrade <leojnxs@gmail.com>
2017-04-05 03:26:30 -0300

    BillingCycle restful API with mongoose schema to persist data

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,28 @@
+const restful = require('node-restful');
+const mongoose = restful.mongoose;
+
+const creditSchema = new mongoose.Schema({
+  name: {type: String, require: true},
+  value: {type: Number, require: true, min: 0},
+});
+
+const debitSchema = new mongoose.Schema({
+  name: {type: String, require: true},
+  value: {type: Number, require: true, min: 0},
+  status: {
+    type: String,
+    require: false,
+    uppercase: true,
+    enum: ['PAGO', 'PENDENTE', 'AGENDADO'],
+  },
+});
+
+const billingCycleShema = new mongoose.Schema({
+  name: {type: String, require: true},
+  month: {type: Number, require: true, min: 1, max: 12},
+  year: {type: Number, require: true, min: 1970, max: 2100},
+  credits: [creditSchema],
+  debits: [debitSchema],
+});
+
+module.exports = restful.model('BillingCycle', billingCycleShema)
