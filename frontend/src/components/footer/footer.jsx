9c58bd39eb1d1e5e36174f65e9659ffc7d70c22d
Leonardo Andrade <leojnxs@gmail.com>
2017-04-09 20:47:44 -0300

    Adding footer

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+  <footer className="main-footer">
+    <strong>Copyright © 2017</strong>
+    <a href="https://github.com/leojnxs" target="_blank"> leojnxs</a>.
+  </footer>
+);
--- frontend/src/containers/app/app.jsx
+++ frontend/src/containers/app/app.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../../config/dependencies'
+
+import '../../config/dependencies';
+
 import Header from '../../components/header/header';
 import SideBar from '../../components/sideBar/sideBar';
+import Footer from '../../components/footer/footer';
 
 export default props => (
-    <div className='wrapper'>
-        <Header />
-        <SideBar />
+  <div className="wrapper">
+    <Header />
+    <SideBar />
+    <div className="content-wrapper">
+      <h1>Content</h1>
     </div>
+    <Footer />
+  </div>
 );
