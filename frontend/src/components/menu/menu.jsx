74596e90669827fe0c535c3df1bc525052d1ef48
Leonardo Andrade <leojnxs@gmail.com>
2017-04-09 20:31:18 -0300

    Ending aside menu components

--- frontend/src/components/menu/menu.jsx
+++ frontend/src/components/menu/menu.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import MenuItem from '../menuItem/menuItem';
+import MenuTree from '../menuTree/menuTree';
 
 export default props => (
-  <ul className="sidebar-menu">
-    <MenuItem path="/#" label="Dashboard" icon="dashboard" />
+  <ul className='sidebar-menu'>
+    <MenuItem path='#' label='Dashboard' icon='dashboard' />
+    <MenuTree label='Registration' icon='edit'>
+      <MenuItem path='#billingCycles' label='Billing Cycles' icon='usd' />
+    </MenuTree>
   </ul>
 );
