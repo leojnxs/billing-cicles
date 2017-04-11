57d16c0107be700b580807aa23ae76b9d0554ffa
Leonardo Andrade <leojnxs@gmail.com>
2017-04-10 22:11:29 -0300

    Adding more components to dashboard page

--- frontend/src/containers/dashboard/dashboard.jsx
+++ frontend/src/containers/dashboard/dashboard.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 
+import Row from '../../components/common/row/row';
 import Content from '../../components/content/content';
 import ContentHeader from '../../components/contentHeader/contentHeader';
+import ValueBox from '../../components/valueBox/valueBox';
 
 class Dashboard extends Component {
   render() {
@@ -9,10 +11,32 @@ class Dashboard extends Component {
       <div>
         <ContentHeader title="Dashboard" small="Versão 1.0" />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 0"
+              text="Valor consolidado"
+            />
+          </Row>
         </Content>
       </div>
-    )
+    );
   }
 }
 
